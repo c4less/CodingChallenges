2047b7da090e26967c89670000de06f42feb25a1
Charles Raj <c4less@users.noreply.github.com>
2022-10-30 20:24:59 -0400

    Time: 178 ms (74.97%), Space: 43.1 MB (47.55%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cs
@@ -0,0 +1,51 @@
+public class Solution {
+    
+        Dictionary<int, List<int>> graphDict = new();
+        HashSet<int> visited = new();
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        
+        for(int i=0; i< numCourses; i++)
+        {
+            if(!graphDict.ContainsKey(i))
+            {
+                graphDict.Add(i, new List<int>());
+            }
+        }
+        
+        foreach(var prereq in prerequisites)
+        {
+            graphDict[prereq[1]].Add(prereq[0]);            
+        }
+         
+       
+        for(int course=0; course< numCourses; course++)    
+        {
+            if(!DFS(course)){
+                return false;
+            }
+        }
+        return true;
+    }
+            
+  
+   
+      public bool DFS(int course)
+    {
+        if(visited.Contains(course)) return false;
+        if(graphDict[course].Count==0) return true;
+        
+        visited.Add(course);
+        foreach(var depCourse in graphDict[course])
+        {
+            if(!DFS(depCourse)){
+                return false;
+            }
+        }
+        visited.Remove(course);
+        graphDict[course]= new();
+        return true;
+    } 
+    
+    
+                
+}
